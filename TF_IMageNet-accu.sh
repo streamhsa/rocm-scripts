2c61811d2958342bc3b7c906199d4f290eea9139
pramenku <7664080+pramenku@users.noreply.github.com>
2019-07-20 18:33:09 +0530

    Add files via upload

--- TF_IMageNet-accu.sh
+++ TF_IMageNet-accu.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#sudo docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add video --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/dockerx:/dockerx  -v $HOME/dataset/imagenet/tf:/imagenet <dockerimage>
-
+#sudo docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add video -v $HOME/dockerx:/dockerx  -v $HOME/dataset/imagenet/tf:/imagenet sunway513/hiptensorflow:rocm2.1-tf1.12-imagenet-eval
 #export HCC_SERIALIZE_KERNEL=0x3
 export  MIOPEN_DEBUG_DISABLE_FIND_DB=1
 export TF_ROCM_FUSION_ENABLE=1
@@ -8,13 +7,13 @@ rocm=346
 #export HSA_ENABLE_SDMA=0
 cd /root
 rm -rf benchmarks-imagenet
-git clone -b cnn_tf_v1.12_compatible https://github.com/tensorflow/benchmarks.git benchmarks-imagenet
+git clone -b cnn_tf_v1.12_compatible https://github.com/tensorflow/benchmarks.git ~/benchmarks-imagenet
 cd /root/benchmarks-imagenet
 #git checkout -b sep7 6a33b4a4b5bda950bb7e45faf13120115cbfdb2f
 #sed -i 's|from tensorflow.contrib import nccl|#from tensorflow.contrib import nccl|g' ./scripts/tf_cnn_benchmarks/variable_mgr.py
 #sed -i 's|import cPickle|import pickle|g' ./scripts/tf_cnn_benchmarks/datasets.py
 
-rm -rf /imagenet/train_benchmarks_resnet50
+#mv -f /imagenet/train_benchmarks_resnet50 /imagenet/train_benchmarks_resnet50-$rocm
 
 #MODEL=( alexnet googlenet inception3 inception4 lenet overfeat resnet50 resnet152_v2 trivial vgg11 vgg16 )
 
@@ -32,7 +31,7 @@ do
 #  --variable_update=parameter_server --local_parameter_device=cpu --num_batches=${ITERATIONS} \
 #  --num_gpus=${NGPUS} --batch_size=${BATCH_SIZE} 2>&1 | tee /dockerx/LongRuns/tf-lr-$i-$BATCH_SIZE-$rocm.txt
 
-python3 scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --data_format=NCHW --batch_size=${BATCH_SIZE} --model=$i --optimizer=momentum --variable_update=parameter_server --local_parameter_device=cpu  --nodistortions --num_gpus=${NGPUS} --num_epochs=${EPOCH} --weight_decay=1e-4 --data_dir=/imagenet --train_dir=/imagenet/train_benchmarks_$i --all_reduce_spec='' --per_gpu_thread_count=1 --save_summaries_steps=1 --summary_verbosity=1 --num_warmup_batches=0 --print_training_accuracy=True --eval=False 2>&1 | tee /dockerx/LongRuns/tf-lr-$i-$BATCH_SIZE-$rocm.txt
+python3 scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --data_format=NCHW --batch_size=${BATCH_SIZE} --model=$i --optimizer=momentum --variable_update=parameter_server --local_parameter_device=cpu  --nodistortions --num_gpus=${NGPUS} --num_epochs=${EPOCH} --weight_decay=1e-4 --data_dir=/imagenet --train_dir=/imagenet/train_benchmarks_$i --all_reduce_spec='' --per_gpu_thread_count=1 --save_summaries_steps=1 --summary_verbosity=1 --num_warmup_batches=0 --print_training_accuracy=True --eval=True 2>&1 | tee /dockerx/LongRuns/tf-lr-$i-$BATCH_SIZE-$rocm-accuracy.txt
 
 done
 
---
+++ tf-xla.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+current=`pwd`
+
+LOGDIR=/dockerx
+BENCHDIR=/root/benchmarks
+BASEDIR=/root
+BUILD=346
+
+download_tensorflow_benchmarks()
+{
+        cd $BASEDIR
+        rm -rf benchmarks
+        git clone -b cnn_tf_v1.12_compatible https://github.com/tensorflow/benchmarks
+}
+
+run_tf_xla_concat_gpu()
+{
+cd /root/tensorflow
+bazel test --compilation_mode=dbg   --cache_test_results=no --config=opt --config=rocm --test_tag_filters=-no_gpu,-benchmark-test,-no_oss,-no_rocm //tensorflow/compiler/xla/tests:concat_test_gpu  --test_output=streamed --verbose_test_summary --verbose_failures 2>&1 | tee $LOGDIR/test-concat_gpu-xla-$BUILD.log
+
+}
+:
+run_tf_xla_cnn_bm()
+{
+    echo "=======================tf_cnn_benchmarks==============="
+        cd $BENCHDIR
+#       sed -i 's/import cPickle/import pickle/g' datasets.py
+        cd $BENCHDIR
+#     MODELS="alexnet"
+    #MODELS="alexnet googlenet inception3 inception4 lenet overfeat resnet50 resnet152_v2 trivial vgg11 vgg16 vgg19 resnet101 resnet50_v1.5"
+        MODELS="resnet50 resnet101"
+		NGPUS=4
+ #       ITERATIONS=50
+#        BATCH_SIZE=( 1 2 4 8 16 32 64 128 )
+#        BATCH_SIZE=64
+
+ #       for j in ${BATCH_SIZE[@]}
+ #       do
+        for i in ${MODELS[@]}
+        do
+    /usr/bin/python3 ./scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --model=$i \
+    --print_training_accuracy=True \
+    --num_gpus=${NGPUS}  --xla 2>&1 | tee $LOGDIR/tfxla-$i-$BUILD.txt
+  #  done
+    done
+
+
+    #grep -E "Model:|total images/sec" tf_cnn_benchmarks_log.txt
+    #Total Avg. Execution time : 300mins
+    # Expected "final" output:
+    # (TITAN X results shown below)
+    #   AlexNet     @   2500    images/sec: 2495.8 +/- 1.3 (jitter = 45.6)      6.178   0.006   0.029
+    #   GoogleNet   @   2500    images/sec: 422.4 +/- 0.2 (jitter = 7.4)        3.647   0.031   0.156
+    #   inception3  @   2500    images/sec: 123.1 +/- 0.1 (jitter = 1.5)        0.436   1.000   1.000
+    #   lenet       @   2550    images/sec: 14362.7 +/- 22.4 (jitter = 827.9)   3.500   0.031   0.156
+    #   overfeat    @   2500    images/sec: 541.1 +/- 0.2 (jitter = 8.9)        3.748   0.031   0.156
+    #   resnet50    @   2500    images/sec: 190.8 +/- 0.0 (jitter = 0.9)        0.912   1.000   1.000
+    #   trivial     @   2500    images/sec: 7949.9 +/- 7.3 (jitter = 354.5)     6.674   0.406   0.406
+    #   vgg11       @   2500    images/sec: 250.6 +/- 0.1 (jitter = 2.0)        4.457   0.031   0.094
+}
+
+
+download_tensorflow_benchmarks
+
+run_tf_xla_concat_gpu
+run_tf_xla_cnn_bm
+
+
