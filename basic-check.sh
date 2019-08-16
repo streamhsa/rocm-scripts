5507262ac6695653f1634055a2f2d3db598680fe
pramenku <7664080+pramenku@users.noreply.github.com>
2019-08-17 01:18:57 +0530

    Update basic-check.sh

--- basic-check.sh
+++ basic-check.sh
@@ -9,17 +9,17 @@ echo "======================running rocminfo=============================" 2>&1 
 /opt/rocm/bin/rocminfo 2>&1 | tee -a $logs/rocminfo.log
 
 echo "======================running hipinfo=============================" 2>&1 | tee $logs/hipinfo.log
-cd $dir/src/hip/samples/1_Utils/hipInfo
+cp -rf /opt/rocm/hip/samples /tmp/
+cd /tmp/samples/1_Utils/hipInfo
 make
 ./hipInfo 2>&1 | tee -a $logs/hipinfo.log
 
 echo "======================running rocm-bandwidth validation=================" 2>&1 | tee $logs/rocm-bw.log
-cd  $dir/bin
-./rocm_bandwidth_test -v 2>&1 | tee -a $logs/rocm-bw.log
+/opt/rocm/bin/rocm_bandwidth_test -v 2>&1 | tee -a $logs/rocm-bw.log
 echo "==============running Unidirectional bandwidth data=================" 2>&1 | tee -a $logs/rocm-bw.log
-./rocm_bandwidth_test -a 2>&1 | tee -a $logs/rocm-bw.log
+/opt/rocm/bin/rocm_bandwidth_test -a 2>&1 | tee -a $logs/rocm-bw.log
 echo "==============running Bidirectional Bandwidth data=================" 2>&1 | tee -a $logs/rocm-bw.log
-./rocm_bandwidth_test -A 2>&1 | tee -a $logs/rocm-bw.log
+/opt/rocm/bin/rocm_bandwidth_test -A 2>&1 | tee -a $logs/rocm-bw.log
 
 
 
