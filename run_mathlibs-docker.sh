1ae39859a1211c0809119ed82b7145a9e26e2c69
pramenku <7664080+pramenku@users.noreply.github.com>
2019-08-18 21:00:25 +0530

    Update run_mathlibs-docker.sh

--- run_mathlibs-docker.sh
+++ run_mathlibs-docker.sh
@@ -9,15 +9,18 @@ ctest --output-on-failure 2>&1 | tee $logs/rocrand-ut.log
 ./benchmark/benchmark_rocrand_generate --engine all --dis all 2>&1 | tee $logs/bm_rocrand_generate.log
 
 cd $dir/rocPRIM/
-#rm -rf build && ./install -ic && cd build
+#rm -rf build && ./install -ic 
+cd build
 ctest --output-on-failure 2>&1 | tee $logs/rocprim.log
 
 cd $dir/rocThrust/
-#rm -rf build && ./install -ic && cd build
+#rm -rf build && ./install -ic 
+cd build
 ctest --output-on-failure 2>&1 | tee $logs/rocthurst.log
 
 cd $dir/hipCUB/
-#rm -rf build && ./install -ic && cd build
+#rm -rf build && ./install -ic
+cd build
 ctest --output-on-failure 2>&1 | tee $logs/hipcub.log
 
 cd $dir
