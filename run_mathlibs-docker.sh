16e9819375f5b79e568e98bd25b751c2e1ab0d50
pramenku <7664080+pramenku@users.noreply.github.com>
2019-08-18 20:42:44 +0530

    Update run_mathlibs-docker.sh

--- run_mathlibs-docker.sh
+++ run_mathlibs-docker.sh
@@ -9,23 +9,23 @@ ctest --output-on-failure 2>&1 | tee $logs/rocrand-ut.log
 ./benchmark/benchmark_rocrand_generate --engine all --dis all 2>&1 | tee $logs/bm_rocrand_generate.log
 
 cd $dir/rocPRIM/
-rm -rf build && ./install -ic && cd build
+#rm -rf build && ./install -ic && cd build
 ctest --output-on-failure 2>&1 | tee $logs/rocprim.log
 
 cd $dir/rocThrust/
-rm -rf build && ./install -ic && cd build
+#rm -rf build && ./install -ic && cd build
 ctest --output-on-failure 2>&1 | tee $logs/rocthurst.log
 
 cd $dir/hipCUB/
-rm -rf build && ./install -ic && cd build
+#rm -rf build && ./install -ic && cd build
 ctest --output-on-failure 2>&1 | tee $logs/hipcub.log
 
 cd $dir
 ./rocFFT/build/release/clients/staging/rocfft-test 2>&1 | tee $logs/rocfft.log
-./rocALUTION/build/release/clients/tests/rocalution-test 2>&1 | tee $logs/rocalution.log
+./rocALUTION/build/release/clients/staging/rocalution-test 2>&1 | tee $logs/rocalution.log
 ./hipBLAS/build/release/clients/staging/hipblas-test 2>&1 | tee $logs/hipblas.log
-./rocSPARSE/build/release/clients/tests/rocsparse-test 2>&1 | tee $logs/rocsparse-test.log
-./hipSPARSE/build/release/clients/tests/hipsparse-test 2>&1 | tee $logs/hipsparse-test.log
+./rocSPARSE/build/release/clients/staging/rocsparse-test 2>&1 | tee $logs/rocsparse-test.log
+./hipSPARSE/build/release/clients/staging/hipsparse-test 2>&1 | tee $logs/hipsparse-test.log
 ./rocBLAS/build/release/clients/staging/rocblas-test --gtest_filter=-*known_bug* 2>&1 | tee $logs/rocblas.log
 
 
