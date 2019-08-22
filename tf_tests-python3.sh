96d453dd1988e043cdd69b071a8182790de4d19f
pramenku <7664080+pramenku@users.noreply.github.com>
2019-08-22 15:29:15 +0530

    Update tf_tests-python3.sh

--- tf_tests-python3.sh
+++ tf_tests-python3.sh
@@ -205,6 +205,28 @@ run_tf_cnn_benchmarks()
     #   vgg11       @   2500    images/sec: 250.6 +/- 0.1 (jitter = 2.0)        4.457   0.031   0.094
 }
 
+run_flower_alexnet()
+{
+    echo "=======================flower-alexnet==============="
+    mkdir -p /dockerx/flower-alexnet
+    rm -rf /dockerx/flower-alexnet/*
+    wget https://raw.githubusercontent.com/pramenku/rocm-scripts/master/preprocessing_factory.py
+    cp -rf preprocessing_factory.py /root/models/research/slim/preprocessing/
+       
+        cd $MODELDIR/research/slim
+     chmod u+x download_and_convert_data.py
+    python3 download_and_convert_data.py --dataset_name=flowers --dataset_dir=/dockerx/flower-alexnet
+   
+     python3 train_image_classifier.py --train_dir=/tmp/flowers-models/alexnet --dataset_name=flowers --dataset_split_name=train --dataset_dir=/dockerx/flower-alexnet --model_name=alexnet_v2 --max_number_of_steps=2000 --batch_size=128 --num_clones=1 2>&1 | tee  flower-alexnet.txt
+
+         cp -rf flower-alexnet.txt $LOGDIR
+
+        #Total Avg. Execution time : 40mins
+        # Expected "final" output:
+        #2018-03-30 04:21:55.142483: I tensorflow/core/kernels/logging_ops.cc:79] eval/Accuracy[0.8526]
+        #2018-03-30 04:21:55.142565: I tensorflow/core/kernels/logging_ops.cc:79] eval/Recall_5[0.9918]
+        #INFO:tensorflow:Finished evaluation at 2018-03-30-04:21:55
+}
 
 #download_tensorflow_models
 #download_tensorflow_benchmarks
@@ -216,6 +238,7 @@ run_resnet_on_cifar10
 run_imagenet_classify
 run_slim_lenet
 run_slim_cifarnet
+run_flower_alexnet
 #run_tf_cnn_benchmarks
 
 
