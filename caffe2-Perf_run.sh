a5d87da981fcdc7d0f073bcf16c2c4601f1abafa
pramenku <7664080+pramenku@users.noreply.github.com>
2019-08-28 23:11:55 +0530

    Update caffe2-Perf_run.sh

--- caffe2-Perf_run.sh
+++ caffe2-Perf_run.sh
@@ -31,43 +31,41 @@ python convnet_benchmarks_dpm.py --model Resnext101  --num_gpus 1 --batch_size 6
 
 
 ###################################2-GPU#################################################
-echo "========================= caffe2 Alexnet 2048 model====================="
-python convnet_benchmarks_dpm.py --model AlexNet  --num_gpus 2 --batch_size 2048 --iterations 10 |& tee -a caffe2-alexnet-1024_2.log
+#echo "========================= caffe2 Alexnet 2048 model====================="
+#python convnet_benchmarks_dpm.py --model AlexNet  --num_gpus 2 --batch_size 2048 --iterations 10 |& tee -a caffe2-alexnet-1024_2.log
 
-echo "========================= caffe2 Inception 256 model====================="
-python convnet_benchmarks_dpm.py --model Inception  --num_gpus 2 --batch_size 256 --iterations 10 |& tee -a caffe2-Inception-128_2.log
+#echo "========================= caffe2 Inception 256 model====================="
+#python convnet_benchmarks_dpm.py --model Inception  --num_gpus 2 --batch_size 256 --iterations 10 |& tee -a caffe2-Inception-128_2.log
 
-echo "========================= caffe2 Resnet50 128 model====================="
-python convnet_benchmarks_dpm.py --model Resnet50  --num_gpus 2 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnet50-64_2.log
+#echo "========================= caffe2 Resnet50 128 model====================="
+#python convnet_benchmarks_dpm.py --model Resnet50  --num_gpus 2 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnet50-64_2.log
 
-echo "========================= caffe2 Resnet101 128 model====================="
-python convnet_benchmarks_dpm.py --model Resnet101  --num_gpus 2 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnet101-64_2.log
+#echo "========================= caffe2 Resnet101 128 model====================="
+#python convnet_benchmarks_dpm.py --model Resnet101  --num_gpus 2 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnet101-64_2.log
 
-echo "========================= caffe2 Resnext101 128 model====================="
-python convnet_benchmarks_dpm.py --model Resnext101  --num_gpus 2 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnext101-64_2.log
+#echo "========================= caffe2 Resnext101 128 model====================="
+#python convnet_benchmarks_dpm.py --model Resnext101  --num_gpus 2 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnext101-64_2.log
 
 
 ###################################4-GPU#################################################
-echo "========================= caffe2 Alexnet 4096 & 256 model====================="
-python convnet_benchmarks_dpm.py --model AlexNet  --num_gpus 4 --batch_size 4096 --iterations 10 |& tee -a caffe2-alexnet-1024.log 
-python convnet_benchmarks_dpm.py --model AlexNet  --num_gpus 4 --batch_size 256 --iterations 10 |& tee -a caffe2-alexnet-64.log
+#echo "========================= caffe2 Alexnet 4096 & 256 model====================="
+#python convnet_benchmarks_dpm.py --model AlexNet  --num_gpus 4 --batch_size 4096 --iterations 10 |& tee -a caffe2-alexnet-1024.log 
+#python convnet_benchmarks_dpm.py --model AlexNet  --num_gpus 4 --batch_size 256 --iterations 10 |& tee -a caffe2-alexnet-64.log
 
-echo "========================= caffe2 Inception  512 & 256 model====================="
-python convnet_benchmarks_dpm.py --model Inception  --num_gpus 4 --batch_size 512 --iterations 10 |& tee -a caffe2-Inception-128.log
-python convnet_benchmarks_dpm.py --model Inception  --num_gpus 4 --batch_size 256 --iterations 10 |& tee -a caffe2-Inception-64.log
+#echo "========================= caffe2 Inception  512 & 256 model====================="
+#python convnet_benchmarks_dpm.py --model Inception  --num_gpus 4 --batch_size 512 --iterations 10 |& tee -a caffe2-Inception-128.log
+#python convnet_benchmarks_dpm.py --model Inception  --num_gpus 4 --batch_size 256 --iterations 10 |& tee -a caffe2-Inception-64.log
 
-echo "========================= caffe2 Resnet50 256 & 128 model====================="
-python convnet_benchmarks_dpm.py --model Resnet50  --num_gpus 4 --batch_size 256 --iterations 10 |& tee -a caffe2-Resnet50-64.log
-python convnet_benchmarks_dpm.py --model Resnet50  --num_gpus 4 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnet50-32.log
+#echo "========================= caffe2 Resnet50 256 & 128 model====================="
+#python convnet_benchmarks_dpm.py --model Resnet50  --num_gpus 4 --batch_size 256 --iterations 10 |& tee -a caffe2-Resnet50-64.log
+#python convnet_benchmarks_dpm.py --model Resnet50  --num_gpus 4 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnet50-32.log
 
-echo "========================= caffe2 Resnet101 256 & 128 model====================="
-python convnet_benchmarks_dpm.py --model Resnet101  --num_gpus 4 --batch_size 256 --iterations 10 |& tee -a caffe2-Resnet101-64.log
-python convnet_benchmarks_dpm.py --model Resnet101  --num_gpus 4 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnet101-32.log
+#echo "========================= caffe2 Resnet101 256 & 128 model====================="
+#python convnet_benchmarks_dpm.py --model Resnet101  --num_gpus 4 --batch_size 256 --iterations 10 |& tee -a caffe2-Resnet101-64.log
+#python convnet_benchmarks_dpm.py --model Resnet101  --num_gpus 4 --batch_size 128 --iterations 10 |& tee -a caffe2-Resnet101-32.log
 
-echo "========================= caffe2 Resnext101 64 & 32 model====================="
-python convnet_benchmarks_dpm.py --model Resnext101  --num_gpus 4 --batch_size 64 --iterations 10 |& tee -a caffe2-Resnext101-64.log
-python convnet_benchmarks_dpm.py --model Resnext101  --num_gpus 4 --batch_size 32 --iterations 10 |& tee -a caffe2-Resnext101-32.log
-
-
+#echo "========================= caffe2 Resnext101 64 & 32 model====================="
+#python convnet_benchmarks_dpm.py --model Resnext101  --num_gpus 4 --batch_size 64 --iterations 10 |& tee -a caffe2-Resnext101-64.log
+#python convnet_benchmarks_dpm.py --model Resnext101  --num_gpus 4 --batch_size 32 --iterations 10 |& tee -a caffe2-Resnext101-32.log
 
 echo "========================end caffe2 benchmark====================="
