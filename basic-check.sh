cc97dc88f79448f49a6a3d1051b9dabcfd391435
pramenku <7664080+pramenku@users.noreply.github.com>
2019-08-28 19:40:36 +0530

    Update basic-check.sh

--- basic-check.sh
+++ basic-check.sh
@@ -13,13 +13,15 @@ echo "======================running romsmi============================="
 /opt/rocm/bin/rocm_smi.py -a
 
 echo "======================running hipinfo=============================" 2>&1 | tee $logs/hipinfo.log
+export LD_LIBRARY_PATH=/opt/rocm/lib
 cp -rf /opt/rocm/hip/samples /tmp/
 cd /tmp/samples/1_Utils/hipInfo
 make
 ./hipInfo 2>&1 | tee -a $logs/hipinfo.log
+unset LD_LIBRARY_PATH
 
-echo "======================running rocm-bandwidth validation=================" 2>&1 | tee $logs/rocm-bw.log
-/opt/rocm/bin/rocm_bandwidth_test -v 2>&1 | tee -a $logs/rocm-bw.log
+#echo "======================running rocm-bandwidth validation=================" 2>&1 | tee $logs/rocm-bw.log
+#/opt/rocm/bin/rocm_bandwidth_test -v 2>&1 | tee -a $logs/rocm-bw.log
 echo "==============running Unidirectional bandwidth data=================" 2>&1 | tee -a $logs/rocm-bw.log
 /opt/rocm/bin/rocm_bandwidth_test -a 2>&1 | tee -a $logs/rocm-bw.log
 echo "==============running Bidirectional Bandwidth data=================" 2>&1 | tee -a $logs/rocm-bw.log
