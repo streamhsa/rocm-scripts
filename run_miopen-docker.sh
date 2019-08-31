133bb04c2a9d8768db22df1b7973ff9b79857aa4
pramenku <7664080+pramenku@users.noreply.github.com>
2019-08-31 19:39:48 +0530

    Update run_miopen-docker.sh

--- run_miopen-docker.sh
+++ run_miopen-docker.sh
@@ -8,8 +8,8 @@ export MIOPEN_CONV_PRECISE_ROCBLAS_TIMING=0
 make clean
 rm -rf *
 CXX=/opt/rocm/hcc/bin/hcc cmake -DMIOPEN_TEST_ALL=ON -DMIOPEN_BACKEND=HIP -DMIOPEN_MAKE_BOOST_PUBLIC=ON -DMIOPEN_TEST_FLAGS="--disable-verification-cache" -DBoost_USE_STATIC_LIBS=Off -DCMAKE_PREFIX_PATH="/opt/rocm/hcc;/opt/rocm/hip" -DCMAKE_CXX_FLAGS="-isystem /usr/include/x86_64-linux-gnu/" .. | tee -a hipmlopen_build.log
-make -j$(nproc) | tee -a hipmlopen_build.log
-make check -j$(nproc) 2>&1| tee $logs/mlopen-ut.log
+make -j$(nproc) | tee -a mlopenhip_build.log
+make check -j$(nproc) 2>&1| tee $logs/mlopenhip-ut.log
 unset MIOPEN_CONV_PRECISE_ROCBLAS_TIMING
 make MIOpenDriver
 ./bin/MIOpenDriver conv -W 341 -H 79 -c 32 -n 4 -k 32 -y 5 -x 10 -p 0 -q 0 -u 2 -v 2 -t 1 -V 0 -i 1 2>&1 | tee -a MIOpenDriver_run.log
@@ -21,8 +21,8 @@ cd $dir/MLOpen/build_ocl
 make clean
 rm -rf *
 cmake -DMIOPEN_TEST_ALL=ON -DMIOPEN_BACKEND=OpenCL -DMIOPEN_MAKE_BOOST_PUBLIC=ON -DBoost_USE_STATIC_LIBS=Off -DMIOPEN_TEST_FLAGS="--disable-verification-cache" -DOPENCL_INCLUDE_DIRS=/opt/rocm/opencl/include/ -DOPENCL_LIBRARIES=/opt/rocm/opencl/lib/x86_64/libamdocl64.so .. | tee -a mlopenocl_build.log
-make -j$(nproc) | tee -a hipmlopen_build.log
-make check -j$(nproc) 2>&1| tee $logs/mlopen-ut.log
+make -j$(nproc) | tee -a mlopenocl_build.log
+make check -j$(nproc) 2>&1| tee $logs/mlopenocl-ut.log
 
 make MIOpenDriver
 ./bin/MIOpenDriver conv -W 341 -H 79 -c 32 -n 4 -k 32 -y 5 -x 10 -p 0 -q 0 -u 2 -v 2 -t 1 -V 0 -i 1 2>&1 | tee -a MIOpenDriver_run.log
