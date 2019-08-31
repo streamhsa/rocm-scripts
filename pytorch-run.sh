d6eaf1b3d11e5588d0d62b0ac4d08891e986b6b5
pramenku <7664080+pramenku@users.noreply.github.com>
2019-08-31 23:28:21 +0530

    Update pytorch-run.sh

--- pytorch-run.sh
+++ pytorch-run.sh
@@ -44,3 +44,54 @@ python /root/pytorch/test_expecttest.py -v 2>&1 | tee $LOGDIR/py-test_expecttest
 
 echo "========================= py-test_docs_coverage====================="
 python /root/pytorch/test_docs_coverage.py -v 2>&1 | tee $LOGDIR/py-test_docs_coverage.log	
+
+echo "========================= py-dist_autograd====================="
+python /root/pytorch/test_dist_autograd.py -v 2>&1 | tee $LOGDIR/py-dist_autograd.log	
+
+echo "========================= py-fake_quant====================="
+python /root/pytorch/test_fake_quant.py -v 2>&1 | tee $LOGDIR/py-fake_quant.log	
+
+echo "========================= py-logging====================="
+python /root/pytorch/test_logging.py -v 2>&1 | tee $LOGDIR/py-logging.log	
+
+echo "========================= py-mkldnn====================="
+python /root/pytorch/test_mkldnn.py -v 2>&1 | tee $LOGDIR/py-mkldnn.log	
+
+echo "========================= py-qat===================="
+python /root/pytorch/test_qat.py -v 2>&1 | tee $LOGDIR/py-qat.log	
+
+
+echo "========================= py-quantization===================="
+python /root/pytorch/test_quantization.py -v 2>&1 | tee $LOGDIR/py-quantization.log	
+
+echo "========================= py-quantized===================="
+python /root/pytorch/test_quantized.py -v 2>&1 | tee $LOGDIR/py-quantized.log	
+
+echo "========================= py-quantized_tensor===================="
+python /root/pytorch/test_quantized_tensor.py -v 2>&1 | tee $LOGDIR/py-quantized_tensor.log	
+
+echo "========================= py-quantized_nn_mods===================="
+python /root/pytorch/test_quantized_nn_mods.py -v 2>&1 | tee $LOGDIR/py-quantized_nn_mods.log	
+
+echo "========================= py-quantizer===================="
+python /root/pytorch/test_quantizer.py -v 2>&1 | tee $LOGDIR/py-quantizer.log	
+
+echo "========================= py-rpc===================="
+python /root/pytorch/test_rpc.py -v 2>&1 | tee $LOGDIR/py-rpc.log	
+
+
+echo "========================= py-namedtuple_return_api===================="
+python /root/pytorch/test_namedtuple_return_api.py -v 2>&1 | tee $LOGDIR/py-namedtuple_return_api.log	
+
+echo "========================= py-jit_fuser===================="
+python /root/pytorch/test_jit_fuser.py -v 2>&1 | tee $LOGDIR/py-jit_fuser.log	
+
+echo "========================= py-tensorboard===================="
+python /root/pytorch/test_tensorboard.py -v 2>&1 | tee $LOGDIR/py-tensorboard.log	
+
+echo "========================= py-namedtensor===================="
+python /root/pytorch/test_namedtensor.py -v 2>&1 | tee $LOGDIR/py-namedtensor.log	
+
+echo "========================= py-jit_disabledr===================="
+python /root/pytorch/test_jit_disabled.py -v 2>&1 | tee $LOGDIR/py-jit_disabled.log	
+
