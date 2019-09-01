fc44936707a26b15935ed1d750a28958af9d81ab
pramenku <7664080+pramenku@users.noreply.github.com>
2019-09-01 14:46:55 +0530

    Update run_mathlibs-docker.sh

--- run_mathlibs-docker.sh
+++ run_mathlibs-docker.sh
@@ -3,11 +3,11 @@ current=`pwd`
 dir=/root/driver
 logs=/dockerx
 
-echo "==============================rocrand=============================="
-cd $dir/rocRAND/build
-ctest --output-on-failure 2>&1 | tee $logs/rocrand-ut.log
-./benchmark/benchmark_rocrand_kernel --engine all --dis all 2>&1 | tee $logs/bm_rocrand_kernal.log
-./benchmark/benchmark_rocrand_generate --engine all --dis all 2>&1 | tee $logs/bm_rocrand_generate.log
+#echo "==============================rocrand=============================="
+#cd $dir/rocRAND/build
+#ctest --output-on-failure 2>&1 | tee $logs/rocrand-ut.log
+#./benchmark/benchmark_rocrand_kernel --engine all --dis all 2>&1 | tee $logs/bm_rocrand_kernal.log
+#./benchmark/benchmark_rocrand_generate --engine all --dis all 2>&1 | tee $logs/bm_rocrand_generate.log
 
 echo "==============================rocprim============================="
 cd $dir/rocPRIM/
@@ -35,19 +35,19 @@ ctest --output-on-failure 2>&1 | tee $logs/hipcub.log
 
 
 
-cd $dir
-echo "==============================rocfft============================="
-./rocFFT/build/release/clients/staging/rocfft-test 2>&1 | tee $logs/rocfft.log
-echo "==============================rocalution============================="
-./rocALUTION/build/release/clients/staging/rocalution-test 2>&1 | tee $logs/rocalution.log
-echo "=============================hipblas============================="
-./hipBLAS/build/release/clients/staging/hipblas-test 2>&1 | tee $logs/hipblas.log
-echo "==============================rocsparse============================="
-./rocSPARSE/build/release/clients/staging/rocsparse-test 2>&1 | tee $logs/rocsparse-test.log
-echo "==============================hipsparse=============================="
-./hipSPARSE/build/release/clients/staging/hipsparse-test 2>&1 | tee $logs/hipsparse-test.log
-echo "==============================rocblas============================="
-./rocBLAS/build/release/clients/staging/rocblas-test --gtest_filter=-*known_bug* 2>&1 | tee $logs/rocblas.log
+#cd $dir
+#echo "==============================rocfft============================="
+#./rocFFT/build/release/clients/staging/rocfft-test 2>&1 | tee $logs/rocfft.log
+#echo "==============================rocalution============================="
+#./rocALUTION/build/release/clients/staging/rocalution-test 2>&1 | tee $logs/rocalution.log
+#echo "=============================hipblas============================="
+#./hipBLAS/build/release/clients/staging/hipblas-test 2>&1 | tee $logs/hipblas.log
+#echo "==============================rocsparse============================="
+#./rocSPARSE/build/release/clients/staging/rocsparse-test 2>&1 | tee $logs/rocsparse-test.log
+#echo "==============================hipsparse=============================="
+#./hipSPARSE/build/release/clients/staging/hipsparse-test 2>&1 | tee $logs/hipsparse-test.log
+#echo "==============================rocblas============================="
+#./rocBLAS/build/release/clients/staging/rocblas-test --gtest_filter=-*known_bug* 2>&1 | tee $logs/rocblas.log
 
 
 
