a187b6396d003b087b341aafe27419fa98ac0e6f
rbudidaamd <49026155+rbudidaamd@users.noreply.github.com>
2019-09-06 17:03:11 +0530

    Update run_mathlibs-docker.sh

--- run_mathlibs-docker.sh
+++ run_mathlibs-docker.sh
@@ -9,32 +9,6 @@ ctest --output-on-failure 2>&1 | tee $logs/rocrand-ut.log
 ./benchmark/benchmark_rocrand_kernel --engine all --dis all 2>&1 | tee $logs/bm_rocrand_kernal.log
 ./benchmark/benchmark_rocrand_generate --engine all --dis all 2>&1 | tee $logs/bm_rocrand_generate.log
 
-echo "==============================rocprim============================="
-cd $dir/rocPRIM/
-rm -rf build && mkdir build
-cd build
-CXX=/opt/rocm/bin/hcc cmake -DBUILD_TEST=ON -DDISABLE_WERROR=ON -DBUILD_BENCHMARK=OFF ../.
-make -j$(nproc)
-ctest --output-on-failure 2>&1 | tee $logs/rocprim.log
-
-echo "==============================rocthrust=============================="
-cd $dir/rocThrust/
-rm -rf build && mkdir build
-cd build
-CXX=/opt/rocm/bin/hcc cmake -DBUILD_TEST=ON ../.
-make -j$(nproc)
-ctest --output-on-failure 2>&1 | tee $logs/rocthurst.log
-
-echo "==============================hipcub=============================="
-cd $dir/hipCUB/
-rm -rf build && mkdir build
-cd build
-CXX=/opt/rocm/bin/hcc cmake -DBUILD_TEST=ON ../.
-make -j$(nproc)
-ctest --output-on-failure 2>&1 | tee $logs/hipcub.log
-
-
-
 cd $dir
 echo "==============================rocfft============================="
 ./rocFFT/build/release/clients/staging/rocfft-test 2>&1 | tee $logs/rocfft.log
@@ -48,6 +22,32 @@ echo "==============================hipsparse=============================="
 ./hipSPARSE/build/release/clients/staging/hipsparse-test 2>&1 | tee $logs/hipsparse-test.log
 echo "==============================rocblas============================="
 ./rocBLAS/build/release/clients/staging/rocblas-test --gtest_filter=-*known_bug* 2>&1 | tee $logs/rocblas.log
+
+
+echo "==============================rocprim============================="
+cd $dir/rocPRIM/
+rm -rf build && mkdir build
+cd build
+CXX=/opt/rocm/bin/hcc cmake -DBUILD_TEST=ON -DDISABLE_WERROR=ON -DBUILD_BENCHMARK=OFF ../.
+make -j$(nproc)
+ctest --output-on-failure 2>&1 | tee $logs/rocprim.log
+
+echo "==============================hipcub=============================="
+cd $dir/hipCUB/
+rm -rf build && mkdir build
+cd build
+CXX=/opt/rocm/bin/hcc cmake -DBUILD_TEST=ON ../.
+make -j$(nproc)
+ctest --output-on-failure 2>&1 | tee $logs/hipcub.log
+
+
+echo "==============================rocthrust=============================="
+cd $dir/rocThrust/
+rm -rf build && mkdir build
+cd build
+CXX=/opt/rocm/bin/hcc cmake -DBUILD_TEST=ON ../.
+make -j$(nproc)
+ctest --output-on-failure 2>&1 | tee $logs/rocthurst.log
 
 
 
